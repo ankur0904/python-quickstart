3d19958fbc44716143fe967f35dcebaee41ef206
ankur0904 <ankursingh91002@gmail.com>
2023-07-27 21:53:31 +0530

    Updates README.md file

--- README.md
+++ README.md
@@ -1,38 +1,40 @@
 ## Quickstart Code
-
+### Replace `<YOUR_NOVU_API_KEY>` with your **API Key**, `<ID>` with **Subscriber ID**
 ```python
 from novu.config import NovuConfig
 from novu.api import EventApi
 from novu.api.subscriber import SubscriberApi
 from novu.dto.subscriber import SubscriberDto
-NovuConfig().configure("https://api.novu.co", "b23c55928da9e76702b74921a7fffee2")
+NovuConfig().configure("https://api.novu.co", "<YOUR_NOVU_API_KEY>")
 
 
+your_subscriber_id = "<ID>" 
 
-your_subscriber_id = "123"
-
-subscriber = SubscriberApi("https://api.novu.co", "b23c55928da9e76702b74921a7fffee2")
+subscriber = SubscriberApi("https://api.novu.co", "<YOUR_NOVU_API_KEY>")
 subscriber.create(SubscriberDto(
         subscriber_id=your_subscriber_id,
-        email="abc@gmail.com",
+        email="<Email>",
         first_name="John",
         last_name="Doe"
     )
 )
 
-subscriber = SubscriberApi("https://api.novu.co", "b23c55928da9e76702b74921a7fffee2")
+
+subscriber = SubscriberApi("https://api.novu.co", "<YOUR_NOVU_API_KEY>")
 subscriber.put(subscriber=SubscriberDto(
-        subscriber_id="123",   # you want update on this id
-        email="abcde@gmail.com",
+        subscriber_id="<ID>",   # you want update on this id
+        email="<New_Updated_Email>",
         first_name="New",
         last_name="Name"
 ))
 
 
-
 EventApi().trigger(
-    name="test",
-    recipients="123",
-    payload={},
+    name="<trigger_ID>",  # The trigger ID of workflow name. It can be found on the workflow page.
+    recipients="<ID>",
+    payload={},  # Your Novu payload goes here
 )
+
+
+print("Done")
 ```
--- main.py
+++ main.py
@@ -2,31 +2,29 @@ from novu.config import NovuConfig
 from novu.api import EventApi
 from novu.api.subscriber import SubscriberApi
 from novu.dto.subscriber import SubscriberDto
-NovuConfig().configure("https://api.novu.co", "b23c55928da9e76702b74921a7fffee2")
+NovuConfig().configure("https://api.novu.co", "<YOUR_NOVU_API_KEY>")
 
 # Create a subscriber
 # This will create a subscriber with the given subscriber_id, email, first_name and last_name
-# check here: https://web.novu.co/subscribers
 
-your_subscriber_id = "123" # Replace this with a unique user ID
+your_subscriber_id = "<ID>" # Replace <ID> with a unique userID
 
-subscriber = SubscriberApi("https://api.novu.co", "b23c55928da9e76702b74921a7fffee2")
+subscriber = SubscriberApi("https://api.novu.co", "<YOUR_NOVU_API_KEY>")
 subscriber.create(SubscriberDto(
         subscriber_id=your_subscriber_id,
-        email="abc@gmail.com",
+        email="<Email>",
         first_name="John",
         last_name="Doe"
     )
 )
 
 # Update subscriber detail
-# This will update the subscriber detail in this case we are changing the email, first_name and last_name
-# Link: https://web.novu.co/subscribers
+# This will update the subscriber detail. In this case we are changing the email, first_name and last_name
 
-subscriber = SubscriberApi("https://api.novu.co", "b23c55928da9e76702b74921a7fffee2")
+subscriber = SubscriberApi("https://api.novu.co", "<YOUR_NOVU_API_KEY>")
 subscriber.put(subscriber=SubscriberDto(
-        subscriber_id="123",   # you want update on this id
-        email="abcde@gmail.com",
+        subscriber_id="<ID>",   # you want update on this id
+        email="<New_Updated_Email>",
         first_name="New",
         last_name="Name"
 ))
@@ -34,8 +32,8 @@ subscriber.put(subscriber=SubscriberDto(
 
 # Trigger the notification
 EventApi().trigger(
-    name="test",  # The trigger ID of workflow name. It can be found on the workflow page.
-    recipients="123",
+    name="<trigger_ID>",  # The trigger ID of workflow name. It can be found on the workflow page.
+    recipients="<ID>",
     payload={},  # Your Novu payload goes here
 )
 
